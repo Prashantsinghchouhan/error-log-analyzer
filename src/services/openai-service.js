1cb177c3bc72708cc283beb9b5bf519908661db7
Prashant Singh Chouhan <90359988+Prashantsinghchouhan@users.noreply.github.com>
2025-07-02 23:47:55 +0530

    Implemented the AppError class for standarization of error

--- src/services/openai-service.js
+++ src/services/openai-service.js
@@ -1,24 +1,46 @@
 const axios = require("axios");
+const AppError = require("../utils/errors/app-error");
 
 async function analyzeLogsWithOpenAI(logText) {
-  console.log("Inside the service line");
   const prompt = `Analyze the following logs and summarize key issues:\n${logText}`;
-  console.log(prompt);
-
-  const response = await axios.post(
-    "https://api.openai.com/v1/chat/completions",
-    {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: prompt }],
-    },
-    {
-      headers: {
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        "Content-Type": "application/json",
+  try {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: prompt }],
       },
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    return response.data.choices[0].message.content;
+  } catch (error) {
+    const status = error.response.status;
+    const openaiError = error.response.data?.error;
+    if (error.response && error.response.status === 429) {
+      throw new AppError({
+        message: "Too many requests. Please wait and retry",
+        statusCode: error.response.status,
+        explanation: [
+          openaiError?.message ||
+            "You've hit the rate limit. Slow down or upgrade your quota.",
+        ],
+      });
     }
-  );
-  return response.data.choices[0].message.content;
+    throw new AppError({
+      message: "OpenAI API error",
+      statusCode: status,
+      explanation: [
+        openaiError?.message ||
+          "An unknown error occurred while calling OpenAI.",
+      ],
+    });
+  }
 }
 
 module.exports = { analyzeLogsWithOpenAI };
